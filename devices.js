4ead4e94785ce0b364b56738979b1c0e34df8d67
Pavel Kovář <Fox@vsetin.org>
2019-12-15 15:56:03 +0100

    Support of IKEA TRADFRI LED1836G9 and E1603/E1702  - IKEA LED1836G9 TRADFRI LED bulb E27 806 lumen, dimmable, warm white  - IKEA E1603/E1702 TRADFRI control outlet

--- devices.js
+++ devices.js
@@ -126,4 +126,40 @@ module.exports = {
       },
     },
   },
+  'ikea.led1836g9': {
+    name: 'TRADFRI bulb E27 WW 806lm',
+        '@type': ['Light', 'OnOffSwitch'],
+    properties: {
+      state: {
+        '@type': 'OnOffProperty',
+        type: 'boolean',
+        fromMqtt: v => v === 'ON',
+        toMqtt: v => (v ? 'ON' : 'OFF'),
+      },
+      brightness: {
+        '@type': 'BrightnessProperty',
+        type: 'number',
+        minimum: 0,
+        maximum: 100,
+        fromMqtt: v => (v / 255) * 100,
+        toMqtt: v => (v / 100) * 255,
+      },
+      linkquality: {
+        type: 'integer',
+        readOnly: true,
+      },
+    },
+  },
+  'ikea.e1603/e1702': {
+    name: 'TRADFRI control outlet',
+        '@type': ['OnOffSwitch'],
+    properties: {
+      state: {
+        '@type': 'OnOffProperty',
+        type: 'boolean',
+        fromMqtt: v => v === 'ON',
+        toMqtt: v => (v ? 'ON' : 'OFF'),
+      },
+    },
+  },
 };
